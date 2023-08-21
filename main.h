4d2e2e6529b461070d50294cbe94de1dbe5d48f1
PereDeMacron <peredemacron@gmail.com>
2023-08-21 06:54:32 -0700

    push

--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 int main(void);
 
--- shell.c
+++ shell.c
@@ -1,26 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
+#include "main.h"
 
 #define MAX_ARGS 10
 
-int main(void)
-{
+int main(void) {
     int flag = 1, status;
-    ssize_t get;
-    size_t buf = 0;
-    pid_t pid;
-    char* args[MAX_ARGS];
-    char *input = NULL;
     int arg_count = 0;
+    int i;
+    char *args[MAX_ARGS];
+    char *input = NULL;
     char *token;
+    size_t buf = 0;
+    ssize_t get;
+    pid_t pid;
+
+    signal(SIGINT, SIG_IGN);
 
     do {
         write(STDOUT_FILENO, "$ ", 2);
         get = getline(&input, &buf, stdin);
+
         if (get == -1)
         {
             free(input);
@@ -28,36 +26,52 @@ int main(void)
         }
 
         arg_count = 0;
+        for (i = 0; i < MAX_ARGS; i++)
+        {
+            args[i] = NULL;
+        }
+
         token = strtok(input, " \t\n");
-    
+
         while (token != NULL && arg_count < MAX_ARGS - 1)
         {
-            args[arg_count] = token;
+            args[arg_count] = strdup(token);
             arg_count++;
             token = strtok(NULL, " \t\n");
         }
         args[arg_count] = NULL;
 
-        pid = fork();
-        if (pid < 0)
+        if (arg_count > 0)
         {
-            perror("fork failed");
-            exit(1);
+            pid = fork();
+            if (pid < 0)
+            {
+                perror("fork failed !");
+                exit(1);
+            }
+            else if (pid == 0)
+            {
+                execvp(args[0], args);
+                perror("execvp failed !");
+                exit(1);
+            }
+            else
+            {
+                waitpid(pid, &status, 0);
+                if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+                    fprintf(stderr, "bash: %s: command not found\n", args[0]);
+                }
+            }
         }
-        else if (pid == 0)
+
+        for (i = 0; i < arg_count; i++)
         {
-            execvp(args[0], args);
-            perror("execvp failed");
-            exit(1);
-        }
-        else
-        {
-            waitpid(pid, &status, 0);
+            free(args[i]);
         }
 
     } while (flag == 1);
 
     free(input);
 
-    return (0);
+    return 0;
 }
