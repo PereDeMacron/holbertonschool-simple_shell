777642a8eb2cf1921f388918e997d71b06753961
PereDeMacron <peredemacron@gmail.com>
2023-08-25 03:08:26 -0700

    test push

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 int main(void) {
     run_shell();
--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 /**
  * handle_command - Execute a command with arguments.
